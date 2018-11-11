48e24ee0a4ef6d3b617ccbfe623ad6a7f7708bc2
Yan Feng <yan.feng@student.tut.fi>
2018-11-11 20:19:06 +0200

    First version_3

--- discussions/views.py
+++ discussions/views.py
@@ -3,7 +3,9 @@ from django.utils.http import urlsafe_base64_decode
 from discussions.models import ChatMessage,Discussion
 from users.models import CustomUser, CustomUserProfile
 from django.core.exceptions import ValidationError
-
+from django.utils import timezone
+from django.shortcuts import render, Http404, HttpResponseRedirect
+from django.http import HttpResponse
 # Create your views here.
 def home(request,uidb64):
     try:
@@ -12,34 +14,34 @@ def home(request,uidb64):
         user_discussions = CustomUser.objects.get(pk=uid).discussions
     except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist, ValidationError):
         user_discussions = None
-    return user_discussions
-
     return render(request, 'discussions/discussion_home.html', {'discussions':user_discussions})
 
 def datail(request,uidb64):
-    try:
-        # urlsafe_base64_decode() decodes to byte string
-        uid = urlsafe_base64_decode(uidb64).decode()
-        discussion = Discussion.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist, ValidationError):
-        discussion = None
-    return discussion
-
-    chats=discussion.chatMessage
-
     if request.method == 'POST':
         post_type = request.POST.get('post_type')
         if post_type == 'send_chat':
             new_chat = ChatMessage.objects.create(
                 content = request.POST.get('content'),
                 user = request.user,
+                headline=timezone.localtime(timezone.now()).strftime("%Y-%m-%d %H:%M:%S"),
             )
             new_chat.save()
             return HttpResponse()
+
         elif post_type == 'get_chat':
             last_chat_id = int(request.POST.get('last_chat_id'))
-            #print last_chat_id
-            chats = ChatMessage.objects.filter(id__gt = last_chat_id)
+
+            try:
+                # urlsafe_base64_decode() decodes to byte string
+                uid = urlsafe_base64_decode(uidb64).decode()
+                discussion = Discussion.objects.get(pk=uid)
+            except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist, ValidationError):
+                discussion = None
+
+            chats = discussion.chatMessage
+            chats = chats.objects.filter(id__gt = last_chat_id)
             return render(request, 'discussions/discussion_detail.html', {'chats': chats})
     else:
         raise Http404
+
+
