d6ee6a05621fbe534eec05ba1e62f84c485a711e
Yan Feng <yan.feng@student.tut.fi>
2018-11-17 22:50:59 +0200

    First version_3

--- discussions/admin.py
+++ discussions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib import admin
 
+from discussions.models import Discussion,ChatMessage
 # Register your models here.
+class DiscussionAdmin(admin.ModelAdmin):
+    exclude = ('date',)
+    list_display = ('topic', 'creator')
+
+class ChatMessageAdmin(admin.ModelAdmin):
+    exclude = ('headline',)
+    list_display = ('user', 'content')
+
+admin.site.register(Discussion, DiscussionAdmin)
+admin.site.register(ChatMessage, ChatMessageAdmin)
