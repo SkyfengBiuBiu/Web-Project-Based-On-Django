dd5524ba45ca58e7652537e12e4d70d2c870b0bb
Yan Feng <yan.feng@student.tut.fi>
2018-11-20 19:04:38 +0200

    First version_3

--- discussions/views.py
+++ discussions/views.py
@@ -1,32 +1,29 @@
-from django.shortcuts import render
-from django.utils.http import urlsafe_base64_decode
-from discussions.models import ChatMessage,Discussion
-from users.models import CustomUser, CustomUserProfile
 from django.core.exceptions import ValidationError
-from django.utils import timezone
-from django.shortcuts import render, Http404, HttpResponseRedirect
 from django.http import HttpResponse
-from django.views import generic
-from .forms import DiscussionCreationForm
-from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 from django.http import JsonResponse
+from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
+from django.views import generic
+
+from discussions.models import ChatMessage, Discussion
+from users.models import CustomUser
+from .forms import DiscussionCreationForm
+
+
 # Create your views here.
-def home(request,user_id):
+def home(request, user_id):
     context = {}
     try:
-        uid = user_id
-        user_discussions=Discussion.objects.filter(users=uid)
-        creator_discussions=Discussion.objects.filter(creator=uid)
-        all_discussions = user_discussions|creator_discussions
-        context['user_id']=uid
+        uid = request.user.id
+        user_discussions = Discussion.objects.filter(users=uid)
+        creator_discussions = Discussion.objects.filter(creator=uid)
+        all_discussions = user_discussions | creator_discussions
+        context['user_id'] = uid
         context['discussions'] = all_discussions
     except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist, ValidationError):
         user_discussions = None
     return HttpResponse(render(request, 'discussions/discussion_home.html', context))
+
 
 # Ajax Post Views
 
@@ -36,29 +33,22 @@ class CreateDiscussionView(generic.CreateView):
     success_url = reverse_lazy('discussions:discussion_home')
 
     def get_success_url(self):
-        id =self.request.user.id
+        id = self.request.user.id
         return reverse_lazy('discussions:discussion_home', kwargs={'user_id': id})
 
     def form_valid(self, form):
-        form.creator=self.request.user
+        form.creator = self.request.user
         return super(CreateDiscussionView, self).form_valid(form)
 
 
-
-class CreateDoneView(generic.TemplateView):
-    template_name = 'discussions/discussion_done.html'
-
-
-
-def detail(request,discussion_id):
-
+def detail(request, discussion_id):
     if request.method == "GET":
 
         chat_list = ChatMessage.objects.filter(discussion=discussion_id).order_by('-id')[:10]
         chat_list = reversed(list(chat_list))
 
-        return render(request, 'discussions/discussion_detail.html', {'chat_list':chat_list,\
-                                                                      'discussion_id':discussion_id})
+        return render(request, 'discussions/discussion_detail.html', {'chat_list': chat_list, \
+                                                                      'discussion_id': discussion_id})
 
     else:
 
@@ -82,40 +72,56 @@ def detail(request,discussion_id):
             id = request.POST.get('id')
             user_id = request.POST.get('user_id')
 
-
             if request.user.id != creator_id:
 
                 if int(user_id) == request.user.id:
-
                     ChatMessage.objects.filter(discussion=discussion_id, user_id=user_id, id=id).delete()
 
             elif request.user.id == creator_id:
 
-                    ChatMessage.objects.filter(discussion=discussion_id, user_id=user_id, id=id).delete()
+                ChatMessage.objects.filter(discussion=discussion_id, user_id=user_id, id=id).delete()
 
+        if request.is_ajax():
+            data = {'success': True}
+            return JsonResponse(data)
         return HttpResponseRedirect('/discussions/%s/detail' % discussion_id)
 
 
-@method_decorator(login_required, name='dispatch')
-class CustomUserLeaveView(generic.DeleteView):
-    template_name = 'users/user_confirm_delete.html'
-    pk_url_kwarg = 'user_id'
-    model = CustomUser
+def post(request, discussion_id):
+    if request.method == "POST":
+        chat_list = ChatMessage.objects.filter(discussion=discussion_id).order_by('-id')[:10]
+        chat_list = reversed(list(chat_list))
+        data = {'chat_list': chat_list,
+                'discussion_id': discussion_id}
+        return JsonResponse(data)
+    else:
+        chat_list = ChatMessage.objects.filter(discussion=discussion_id).order_by('-id')[:10]
+        chat_list = reversed(list(chat_list))
+        return render(request, 'discussions/chat_message_list.html',
+                      context={'chat_list': chat_list, 'discussion_id': discussion_id})
 
-    def get_success_url(self):
-        return reverse_lazy('users:delete_done')
+
+def post_fom(request):
+    return render(request, 'discussions/chat_message_form.html')
 
 
-def leave(request,discussion_id,user_id):
+def leave(request, discussion_id, user_id):
     context = {}
     try:
         discussion = Discussion.objects.get(pk=discussion_id)
-        user=CustomUser.objects.get(pk=user_id)
+        user = CustomUser.objects.get(pk=user_id)
         discussion.users.remove(user)
         Discussion.objects.filter(users__isnull=True).delete()
-        discussion = Discussion.objects.filter(users=user_id)
+
+        if (discussion.creator == user):
+            discussion.delete()
+        Discussion.objects.filter(users__isnull=True).delete()
+
+        user_discussions = Discussion.objects.filter(users=user_id)
+        creator_discussions = Discussion.objects.filter(creator=user_id)
+        all_discussions = user_discussions | creator_discussions
         context['user_id'] = user_id
-        context['discussions'] = discussion
+        context['discussions'] = all_discussions
     except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist, ValidationError):
         discussion = None
     return HttpResponse(render(request, 'discussions/discussion_home.html', context))
