236655185a4bbd31ff5f85b5cf7e4ed098b137cc
Xie Huang <xieh@student.tut.fi>
2018-11-25 06:26:54 +0800

    1.Fix bug: jquery .on().

--- discussions/models.py
+++ discussions/models.py
@@ -1,13 +1,13 @@
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 from users.models import CustomUser
-from datetime import datetime
+
 
 # Create your models here.
 class ChatMessage(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    headline = models.DateTimeField(default=datetime.now, blank=True)
+    headline = models.DateTimeField(auto_now=True, blank=True)
     content = models.CharField(max_length=200)
 
     def __str__(self):
@@ -19,11 +19,11 @@ class ChatMessage(models.Model):
 
 class Discussion(models.Model):
     """ Write your answer in 7.1 here. """
-    topic = models.CharField(max_length=200,default='family', blank=False, null=False)
-    creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name='creator')
-    users = models.ManyToManyField(CustomUser,related_name='users')
+    topic = models.CharField(max_length=200, default='family', blank=False, null=False)
+    creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='creator')
+    users = models.ManyToManyField(CustomUser, related_name='users')
     chatMessage = models.ManyToManyField(ChatMessage)
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    date = models.DateTimeField(auto_now=True, blank=True)
 
     class Meta:
         ordering = ["date"]
